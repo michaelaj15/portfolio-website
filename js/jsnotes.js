b129f43eaa0d9d22e5be31fc68b65491b4cc93a8
michaelaj15 <73558301+michaelaj15@users.noreply.github.com>
2022-01-04 17:33:25 -0700

    CSSChanges

---
+++ js/jsnotes.js
@@ -0,0 +1,93 @@
+//STRINGS
+//var text1= "Hello";
+//var text2 = "Hello, spaces are also OK!";
+//var text3 = 'Single quotes are valid too.';
+
+//var escapedText1 = "He said: \"Yes!\", that's for sure";
+//var escapedText2 = 'He said: "Yes!", that\'s for sure!';
+
+//NUMBERS
+//var numberInteger = 456;
+//var numberInteger2 = -345 //This is negative integer
+//var numberFloat = 50.25;
+
+//BOOLEANS
+//var boolean1 = true; //don't use quotes
+//var boolean2 = false;
+//var notABoolean = "false"; //This is just the string "false", not false!
+
+//NULL AND UNDEFINED // don't use quotes
+//console.log(thisVariableDoesntExist); //Will output undefined
+//var emptyVariable = null;
+//var notNullButAString = "null";
+//console.log(doesntExist); // Outputs undefined
+//console.log(emptyVariable); // Outputs null
+//console.log(notNullButAString); //Outputs "null" - not the same as null!
+
+//OBJECTS
+//var person = {
+  //name: "John",
+  //age: 39
+//};
+
+//var age = 39;
+//var name = "John";
+//var personParent = {
+  //name: name,
+  //age: age,
+  //child: {
+    //name: "Sara"
+  //}
+//};
+
+//personParent.name = "Anne";
+//personParent.child.size = 175;
+//console.log(personParent.child.name);
+
+//console.log(child.personParent.name);
+
+//var person = {
+  //name: "Michaela",
+  //age: 24,
+  //hobbies: "downhill skiing, ukulele, writing"
+//}
+
+//ARRAYS
+//var shortArray = [1,2,3];
+//var mixedArray = [
+//  1,
+//  "Hello",
+//  {},
+//  ["nested array"]
+//];
+
+//FUNCTIONS / METHOD
+//function add(number1, number2) {
+  //return number1 + number2;
+//}
+
+//var result = add(10, 100);
+
+//function add(number1, number2) {
+  //Do something in here
+//}
+
+//var result = add(10,100);
+
+//function add(number1, number2) {
+  //return number1 + number2;
+//}
+
+//var result=add(10,100);
+
+//CONDITIONALS
+//if (age === 20) {
+//  console.log("You are 20 years old!");
+//} else {
+  //console.log("You are not 20 years old...");
+//}
+
+//var myCondition = true;
+//if (myCondition) {
+  //do something
+//}
